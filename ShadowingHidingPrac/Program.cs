3f28b64d5a737ea18cc307843c334ff27891cfb8
Sooyeon Lim <96305365+printyeon@users.noreply.github.com>
2023-06-12 13:34:55 +0900

    변수에서의 하이딩

--- ShadowingHidingPrac/Program.cs
+++ ShadowingHidingPrac/Program.cs
@@ -8,11 +8,27 @@ namespace ShadowingHidingPrac
 {
     internal class Program
     {
+        class Paraent
+        {
+            public int variable = 273;
+        }
+        class Child : Paraent {
+            public new string variable = "shadowing";
+        }
+
         public static int number = 10;
         static void Main(string[] args)
         {
+            //shadowing
             int number = 20;
-            Console.WriteLine(number);
+            Console.WriteLine(number);//20
+
+            //hiding
+            Child child = new Child();
+            Console.WriteLine(child.variable);//"shadowing"
+
+            Paraent p = child;
+            Console.WriteLine(p.variable);//"273"
         }
     }
 }
