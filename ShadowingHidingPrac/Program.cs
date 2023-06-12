3b96a8dd512e6d549dbaf14403f19e91c1cc40e3
Sooyeon Lim <96305365+printyeon@users.noreply.github.com>
2023-06-12 13:37:28 +0900

    메서드 하이딩 예제

--- ShadowingHidingPrac/Program.cs
+++ ShadowingHidingPrac/Program.cs
@@ -11,24 +11,36 @@ namespace ShadowingHidingPrac
         class Paraent
         {
             public int variable = 273;
+            public void Method()
+            {
+                Console.WriteLine("부모의 메서드");
+            }
         }
+
         class Child : Paraent {
-            public new string variable = "shadowing";
+            public new string variable = "shadowing"; // new를 쓰면 hiding이라는 것을 명시함
+            public new void Method() //hiding / overridingX
+            {
+                Console.WriteLine("자식의 메서드");
+            }
         }
 
         public static int number = 10;
         static void Main(string[] args)
         {
-            //shadowing
+            //shadowing - 상속X
             int number = 20;
             Console.WriteLine(number);//20
 
-            //hiding
+            //hiding - 상속
             Child child = new Child();
             Console.WriteLine(child.variable);//"shadowing"
 
             Paraent p = child;
             Console.WriteLine(p.variable);//"273"
+
+            child.Method();
+            p.Method();
         }
     }
 }
